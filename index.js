8547a54de1a2e412c6c24effcb54d583ad193dca
Rodrigo Manzo <rodrigoj.manzo@gmail.com>
2023-03-21 00:37:32 -0300

    agrego swagger al proyecto

--- index.js
+++ index.js
@@ -7,10 +7,26 @@ import {PassportAuth} from './src/middlewares/index.js'
 import passport from "passport";
 import cookieParser from 'cookie-parser'
 import _dirname from "./src/dirname.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from 'swagger-ui-express'
 
+const swaggerOptions ={
+  definition:{
+    openapi: "3.0.0",
+    info:{
+      title: "ENTREGA FINAL CODERHOUSE CURSO BACKEND",
+      description:"Aplicacion en Express, Modelo Vista Controlador, documentada en Swagger, con un interfas en GraphQL y persistencia en MongoDB"
+    },
+  },
+  apis: ["./src/docs/**/*.yaml"],
+}
 
 
 const app = express();
+
+const swaggerSpecs = swaggerJSDoc(swaggerOptions)
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs))
+
 app.use(cookieParser())
 PassportAuth.init()
 app.use(session(
@@ -31,17 +47,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth",AuthRouter)
 app.use("/api/products", ProductRouter);
 app.use("/api/cart", CartRouter);
-app.get("/", (req,res)=>{
-  res.render("home")
-})
-app.get("/login", (req, res)=>{
-  res.render('login')
-})
 
-app.get("/signup", (req, res)=>{
-  res.render('signup')
-})
+// app.get("/", (req,res)=>{
+//   res.render("home")
+// })
+// app.get("/login", (req, res)=>{
+//   res.render('login')
+// })
+
+// app.get("/signup", (req, res)=>{
+//   res.render('signup')
+// })
 
 const server = app.listen(config.SERVER.PORT, () =>
-  console.log(`Server running on port ${server.address().port}`)
+  console.log(`Server running on port ${server.address().port} /// Info avaliable on /api/docs`)
 );
